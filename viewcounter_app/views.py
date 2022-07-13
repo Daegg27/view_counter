5e85f2fc52e99dddd59f4f6fc73680a045f61451
Dalton Eggleston <egglestondalton@yahoo.com>
2022-07-13 12:38:42 -0700

    done

--- viewcounter_app/views.py
+++ viewcounter_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+import random
+
+
+users = {}
 
 # Create your views here.
 def index(request):
 
+    user_id = request.COOKIES.get('user_id')
 
-    return render(request, 'index.html')
+    if user_id not in users:
+        user_id = str(random.randrange(1000, 600000))
+        users[user_id] = {
+            'count': 1
+        }
+    else:
+        users[user_id]['count'] += 1
+
+    my_data = {
+        'user_id': user_id,
+        'view_count': users[user_id]['count']
+    }
+
+    response = render(request, 'index.html', my_data)
+    response.set_cookie('user_id', user_id)
+
+    return response
